949d737f9327f1ac0056b8b7d8aad05b847f72b7
donpai2000-afk <donpai2000@googlemail.com>
2026-02-14 13:01:50 +0900

    update sw to v6

--- public/sw.js
+++ public/sw.js
@@ -23,11 +23,5 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      const keys = await caches.keys();
-      await Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))));
-      await self.clients.claim();
-    })()
-  );
+  event.waitUntil(self.clients.claim());
 });
